d7b9ee943cac9533e041e05cbb77b08c2fa763b9
Venus <copilot5527@outlook.com>
2024-05-03 03:33:13 -0500

    added logic run f func

---
+++ target/debug/deps/minigrep-46c773144ad78de0.d
@@ -0,0 +1,5 @@
+/home/venus/solana/minigrep/target/debug/deps/minigrep-46c773144ad78de0: src/main.rs
+
+/home/venus/solana/minigrep/target/debug/deps/minigrep-46c773144ad78de0.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/venus/solana/minigrep/target/debug/minigrep: /home/venus/solana/minigrep/src/main.rs

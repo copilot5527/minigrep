71f557d7bc8677cbc63fc822cd649825b0723fd6
Venus <copilot5527@outlook.com>
2024-05-03 03:18:57 -0500

    first

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,9 @@ fn main() {
 
     println!("With text:\n{contents}");
 }
-// fn parse_config(args: &[String]) -> (&str, &str) {
-//     let query = &args[1];
-//     let file_path = &args[2];
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
 
-//     (query, file_path)
-// }
+    (query, file_path)
+}
